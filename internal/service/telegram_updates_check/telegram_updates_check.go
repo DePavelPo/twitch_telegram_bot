de29f3fba4d881c5392ea2f3cff9daa55a21ee43
Pavel Muslimov <pavel.muslimov98@gmail.com>
2022-06-22 23:47:22 +0300

    telegram updates check process added

---
+++ internal/service/telegram_updates_check/telegram_updates_check.go
@@ -0,0 +1,73 @@
+package telegram_updates_check
+
+import (
+	"context"
+	"os"
+	"time"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/sirupsen/logrus"
+)
+
+const (
+	telegramUpdatesCheckBGSync = "telegramUpdatesCheck_BGSync"
+)
+
+type TelegramUpdatesCheckService struct {
+}
+
+func NewTelegramUpdatesCheckService() *TelegramUpdatesCheckService {
+	return &TelegramUpdatesCheckService{}
+}
+
+func (tmcs *TelegramUpdatesCheckService) Sync(ctx context.Context) error {
+
+	bot, err := tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_API_TOKEN"))
+	if err != nil {
+		return err
+	}
+
+	bot.Debug = true
+
+	logrus.Printf("Authorized on account %s", bot.Self.UserName)
+
+	reader := tgbotapi.NewUpdate(0)
+	reader.Timeout = 60
+
+	updates := bot.GetUpdatesChan(reader)
+
+	for updateInfo := range updates {
+		if updateInfo.Message != nil {
+			logrus.Printf("[%s] %s", updateInfo.Message.From.UserName, updateInfo.Message.Text)
+
+			msg := tgbotapi.NewMessage(updateInfo.Message.Chat.ID, updateInfo.Message.Text)
+			msg.ReplyToMessageID = updateInfo.Message.MessageID
+
+			bot.Send(msg)
+		}
+	}
+
+	return nil
+}
+
+func (tmcs *TelegramUpdatesCheckService) SyncBg(ctx context.Context, syncInterval time.Duration) {
+	ticker := time.NewTicker(syncInterval)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ctx.Done():
+			logrus.Infof("stoping bg %s process", telegramUpdatesCheckBGSync)
+			return
+		case <-ticker.C:
+			logrus.Infof("started bg %s process", telegramUpdatesCheckBGSync)
+			err := tmcs.Sync(ctx)
+			if err != nil {
+				logrus.Info("could not check telegram updates")
+				continue
+			}
+			logrus.Info("telegram updates check was complited")
+		}
+	}
+
+}
