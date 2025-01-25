2b32e623b8ec5a01c1b488cb0e6ea3b94357ecf5
DePavelPo <pavel.muslimov98@gmail.com>
2025-01-25 19:22:34 +0300

    small optimizations for throwing notification

--- internal/service/notification/twitch.go
+++ internal/service/notification/twitch.go
@@ -2,8 +2,10 @@ package notification
 
 import (
 	"context"
+	"slices"
 	"strconv"
 	"time"
+
 	"twitch_telegram_bot/internal/models"
 
 	"github.com/pkg/errors"
@@ -91,39 +93,10 @@ func (tn *TwitchNotificationService) Sync(ctx context.Context) error {
 					}
 
 					if streams != nil {
-						for _, streamInfo := range streams.StreamInfo {
 
-							tx, err := tn.dbRepo.BeginTransaction(ctx)
-							if err != nil {
-								return errors.Wrap(err, "BeginTransaction")
-							}
-
-							streamIdInt, err := strconv.ParseUint(streamInfo.StreamId, 10, 64)
-							if err != nil {
-								logrus.Errorf("cannot parse %s to uint64", streamInfo.StreamId)
-								tx.Rollback()
-								continue
-							}
-
-							err = tn.dbRepo.AddTwitchNotificationLog(ctx, tx, streamIdInt, notification.ChatId)
-							if err != nil {
-								logrus.Errorf("cannot add notification log for %d", streamIdInt)
-								tx.Rollback()
-								continue
-							}
-
-							if currentTime.Before(streamInfo.StartedAt.Add(time.Minute * 10)) {
-								err = tn.ThrowNotification(ctx, streamInfo, notification.ChatId)
-								if err != nil {
-									tx.Rollback()
-									return errors.Wrap(err, "ThrowNotification")
-								}
-							}
-
-							if err = tx.Commit(); err != nil {
-								return errors.Wrap(err, "Commit")
-							}
-
+						err = tn.LogAndThrowNotifications(ctx, streams.StreamInfo, notification.TwitchUser, notification.ChatId, currentTime)
+						if err != nil {
+							return errors.Wrap(err, "LogAndThrowNotifications")
 						}
 
 					}
@@ -141,50 +114,67 @@ func (tn *TwitchNotificationService) Sync(ctx context.Context) error {
 
 		if streams != nil {
 			for _, notification := range notifications {
-				for _, streamInfo := range streams.StreamInfo {
-					if notification.TwitchUser == streamInfo.UserId ||
-						notification.TwitchUser == streamInfo.UserLogin ||
-						notification.TwitchUser == streamInfo.UserName {
-
-						tx, err := tn.dbRepo.BeginTransaction(ctx)
-						if err != nil {
-							return errors.Wrap(err, "BeginTransaction")
-						}
-
-						streamIdInt, err := strconv.ParseUint(streamInfo.StreamId, 10, 64)
-						if err != nil {
-							logrus.Errorf("cannot parse %s to uint64", streamInfo.StreamId)
-							tx.Rollback()
-							continue
-						}
-
-						err = tn.dbRepo.AddTwitchNotificationLog(ctx, tx, streamIdInt, notification.ChatId)
-						if err != nil {
-							logrus.Errorf("cannot add notification log for %d", streamIdInt)
-							tx.Rollback()
-							continue
-						}
-
-						if currentTime.Before(streamInfo.StartedAt.Add(time.Minute * 10)) {
-
-							err = tn.ThrowNotification(ctx, streamInfo, notification.ChatId)
-							if err != nil {
-								tx.Rollback()
-								return errors.Wrap(err, "ThrowNotification")
-							}
-
-						}
-
-						if err = tx.Commit(); err != nil {
-							return errors.Wrap(err, "Commit")
-						}
-
-					}
-
+				err = tn.LogAndThrowNotifications(ctx, streams.StreamInfo, notification.TwitchUser, notification.ChatId, currentTime)
+				if err != nil {
+					return errors.Wrap(err, "LogAndThrowNotifications")
 				}
 			}
 
 		}
 
 	}
+}
+
+// log info about the notification to db and throw the notification to the bot
+func (tn *TwitchNotificationService) LogAndThrowNotifications(
+	ctx context.Context,
+	streamsInfo []models.Stream,
+	twitchUser string,
+	chatID uint64,
+	currentTime time.Time,
+) error {
+
+	for _, streamInfo := range streamsInfo {
+		if slices.Contains([]string{streamInfo.UserId, streamInfo.UserLogin, streamInfo.UserName}, twitchUser) {
+
+			tx, err := tn.dbRepo.BeginTransaction(ctx)
+			if err != nil {
+				return errors.Wrap(err, "BeginTransaction")
+			}
+
+			streamIdInt, err := strconv.ParseUint(streamInfo.StreamId, 10, 64)
+			if err != nil {
+				logrus.Errorf("cannot parse %s to uint64", streamInfo.StreamId)
+				tx.Rollback()
+				continue
+			}
+
+			err = tn.dbRepo.AddTwitchNotificationLog(ctx, tx, streamIdInt, chatID)
+			if err != nil {
+				logrus.Errorf("cannot add notification log for %d", streamIdInt)
+				tx.Rollback()
+				continue
+			}
+
+			// throw notifications for streams
+			// that started less then 10 minutes ago
+			if currentTime.Before(streamInfo.StartedAt.Add(time.Minute * 10)) {
+
+				err = tn.ThrowNotification(ctx, streamInfo, chatID)
+				if err != nil {
+					tx.Rollback()
+					return errors.Wrap(err, "ThrowNotification")
+				}
+
+			}
+
+			if err = tx.Commit(); err != nil {
+				return errors.Wrap(err, "Commit")
+			}
+
+		}
+
+	}
+
+	return nil
 }
